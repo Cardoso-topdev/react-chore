37d2a6fc99532684769b538dbd8154abf45f891a
Cardoso-topdev <Oscar@cardoso-devstar.com>
2021-01-29 07:35:59 -0600

    update

--- src/pages/privacy/index.jsx
+++ src/pages/privacy/index.jsx
@@ -7,9 +7,9 @@ import Footer from '../../components/footer'
 
 const Privacy = () => <div className="privacy container">
   <Navigation isColorWhite={false} />
-  <SectionTitle extraCls="wow fadeInLeft" header="Privacy Statement for Credit Academy" />
+  <SectionTitle header="Privacy Statement for Credit Academy" />
   {ACC_DATA.map( (item, idx) => (
-    <div className="accordion-item primary-border wow fadeInDown" key={idx} data-wow-delay={idx*0.1 + "s"}>
+    <div className="accordion-item primary-border" key={idx}>
       <dt className="accordion-item__title sub-title" data-toggle="collapse" data-target={"#item" + idx} aria-expanded="false" aria-controls="faq1">
         {item.title}
       </dt>
